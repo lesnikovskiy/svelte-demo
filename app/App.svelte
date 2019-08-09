bdfaac51b749fee3512a70ad0c3a0fa8aac53431
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2019-08-09 09:43:44 +0300

    display users from github

--- app/App.svelte
+++ app/App.svelte
@@ -1,5 +1,42 @@
 <script>
-    let name = 'world';
+    import { onMount } from "svelte";
+    import User from "./User.svelte";
+
+    let users;
+
+    function getGithubUsers() {
+        fetch('https://api.github.com/users')
+            .then(resp => resp.json())
+            .then(data => (users = data));
+    }
+
+    onMount(() => {
+        getGithubUsers();
+    });
+
 </script>
 
-<h1>Hello {name}!</h1>
+<style>
+    .user-list {
+        display: flex;
+        flex-flow: wrap;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    .user-list li {
+        width: 20%;
+        padding: 10px;
+    }
+</style>
+
+<main>
+    {#if users}
+        <ul class="user-list">
+            {#each users as user}
+                <User username={user.login} avatar={user.avatar_url} />
+            {/each}
+        </ul>
+    {/if}
+</main>
